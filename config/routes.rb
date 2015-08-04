daecb75c50148de44423fd9611beaf7c7fa5fcc4
Chris Brucher <c.p.brucher@gmail.com>
2015-08-04 14:07:57 +0000

    Added Contacts framework - if needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+  resources :contacts
   resources :users
   root "pages#welcome"
   # The priority is based upon order of creation: first created -> highest priority.
