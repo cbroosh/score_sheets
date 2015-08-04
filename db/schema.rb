c2c380352d0401edb1504ca1e9e5b434071290c1
Chris Brucher <c.p.brucher@gmail.com>
2015-08-04 14:41:10 +0000

    Added validations for users email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804140356) do
+ActiveRecord::Schema.define(version: 20150804143709) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -27,5 +27,7 @@ ActiveRecord::Schema.define(version: 20150804140356) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
