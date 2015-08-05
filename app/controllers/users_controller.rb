6bd22deeff038a6592390417f0988d2e10b5fb45
Chris Brucher <c.p.brucher@gmail.com>
2015-08-05 17:39:53 +0000

    Debugging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,27 @@
 class UsersController < ApplicationController
+    def show
+       @user = User.find(params[:id]) 
+    end
+    
     def new 
-        
+        @user = User.new
     end
     
     def create
-        
+        @user = User.new(params[user_params])
+        if @user.save
+            flash[:success] = "Welcome aboard!"
+            redirect_to @user
+        else
+            flash[:danger] = "Something went wrong."
+            render 'new'
+        end
     end
+    
+    
+    
+    private
+        def user_params
+           params.require(:user).permit(:name, :email, :password, :password_confirmation) 
+        end
 end
